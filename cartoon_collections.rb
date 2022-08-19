c30d8fd34d4799c67db4afb749b034372b8eca82
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-08-20 00:38:00 +0300

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(shorties) #code an argument here
   # Your code here
+    shorties.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(shorties) # code an argument here
   # Your code here
+  shorties_caps = []
+  shorties.each do |dwarf|
+    shorties_caps << (dwarf.capitalize + "!" )
+  end
+  return shorties_caps
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls shorties #code an argument here
   # Your code here
+  shorties.any? do |dwarf|
+    dwarf.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese cheese_available   # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_available.find {|cheese| cheese_types.include?(cheese)}
+
 end
